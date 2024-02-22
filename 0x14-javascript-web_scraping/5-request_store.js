80434ca5bd086918b845a8e0f5baf6db3f941a15
Ritapossible <egwuaturita95@gmail.com>
2024-02-22 14:02:55 -0800

    Web scrapping

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const request = require('request');
+const fs = require('fs');
+
+request(process.argv[2], function (error, response, body) {
+  if (error == null) {
+    fs.writeFileSync(process.argv[3], body);
+  }
+});
